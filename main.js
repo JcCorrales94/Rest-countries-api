81f665ea362c39f6d78fd79eb531e11ecd85bef7
JcCorrales94 <jccorrales94@gmail.com>
2023-01-07 20:14:54 +0100

    solucionando fallos

--- main.js
+++ main.js
@@ -26,3 +26,20 @@ fetch('https://restcountries.com/v3.1/all')
     })
   })
   .catch((error) => { console.error(error) })
+
+// selectRegion.addEventListener('change', (data) => {
+//   if (selectRegion.value === data.region) {
+//     const article = document.createElement('article')
+//     article.classList.add('region', 'flex', 'flex-col', 'justify-between', 'rounded-xl', 'bg-[rgb(43,55,67)]', 'overflow-auto', 'w-56', 'h-100', 'my-14', 'mx-20')
+//     article.innerHTML = ''
+//     article.innerHTML = `
+//     <div class="p-5 flex flex-col gap-4 text-lg text-white">
+//     <p class="font-bold">${data.name.common}</p>
+//     <p class="text-sm">Population: <span class="text-slate-300">${data.population}</span></p>
+//     <p class="text-sm">Region: <span class="text-slate-300">${data.region}</span></p>
+//     <p class="text-sm">Capital: <span class="text-slate-300">${data.capital}</span></p>
+//   </div>
+//   `
+//     boxRegion.append(article)
+//   }
+// })
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,12 @@
 module.exports = {
   content: [
 "./index.html",
-"./src/**/*.{js,ts,jsx,tsx}",
+"./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
     require('@tailwindcss/forms'),
-  ],
+  ]
 }
